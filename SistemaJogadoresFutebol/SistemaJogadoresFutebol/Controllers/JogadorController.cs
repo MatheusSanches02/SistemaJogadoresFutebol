7b032b9c8de36279f726c6c20545bba235a22ca2
MatheusSanches02 <rm87426@fiap.com.br>
2022-09-27 19:07:58 -0300

    First Commit

---
+++ SistemaJogadoresFutebol/SistemaJogadoresFutebol/Controllers/JogadorController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaJogadoresFutebol.Controllers
+{
+    public class JogadorController : Controller
+    {
+        // GET: JogadorController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: JogadorController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: JogadorController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: JogadorController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: JogadorController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: JogadorController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: JogadorController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: JogadorController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SistemaJogadoresFutebol/SistemaJogadoresFutebol/Models/Jogador.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaJogadoresFutebol.Models
+{
+    public class Jogador
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public int Idade { get; set; }
+        public string? Nacionalidade { get; set; }
+        public DateTime Nascimento { get; set; }
+
+        [Display(Name = "Liga Disputada")]
+        public string? LigaDisputada { get; set; }
+        public bool Lesionado { get; set; }
+
+        [Display(Name = "Posição")]
+        public Posicao Posicao { get; set; }
+    }
+
+    public enum Posicao
+    {
+        Goleiro, 
+        Defensor,
+        [Description("Meio Campo")]
+        MeioCampo, 
+        Atacante 
+    }
+}
